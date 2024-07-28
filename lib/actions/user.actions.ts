b217351cd380014d028335a2f736cac210e8d50a
PedroHenriqueNS <106723520+PedroHenriqueNS@users.noreply.github.com>
2024-07-28 16:29:44 -0300

    finalize the app

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { ID } from "node-appwrite"
+import { ID, Query } from "node-appwrite"
 import { createAdminClient, createSessionClient } from "../appwrite"
 import { cookies } from "next/headers"
 import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils"
@@ -15,19 +15,43 @@ const {
     APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID
 } = process.env
 
+export const getUserInfo = async ({ userId }: getUserInfoProps) => {
+    try {
+        const { database } = await createAdminClient()
+
+        const user = await database.listDocuments(
+            DATABASE_ID!,
+            USER_COLLECTION_ID!,
+            [Query.equal('userId', [userId])]
+        )
+
+        return parseStringify(user.documents[0])
+    } catch (error) {
+        console.log('An error occurred while getting all user banks', error)
+    }
+}
+
 export const signIn = async ({ email, password }: signInProps) => {
     try {
         const { account } = await createAdminClient()
+        const session = await account.createEmailPasswordSession(email, password)
 
-        const response = await account.createEmailPasswordSession(email, password)
+        cookies().set('appwrite-session', session.secret, {
+            path: '/',
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true
+        })
 
-        return parseStringify(response)
+        const user = await getUserInfo({ userId: session.userId })
+
+        return parseStringify(user)
     } catch (error) {
         console.error("Error", error)
     }
 }
 
-export const signUp = async ({ password, ...userData}: SignUpParams) => {
+export const signUp = async ({ password, ...userData }: SignUpParams) => {
     const { email, firstName, lastName } = userData;
 
     let newUserAccount
@@ -43,14 +67,14 @@ export const signUp = async ({ password, ...userData}: SignUpParams) => {
         )
 
         if (!newUserAccount) throw new Error('Error creating user')
-        
+
         const dwollaCustomerUrl = await createDwollaCustomer({
             ...userData,
             type: 'personal'
         })
 
         if (!dwollaCustomerUrl) throw new Error('Error creating Dwolla customer')
-        
+
         const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl)
 
         const newUser = await database.createDocument(
@@ -84,7 +108,9 @@ export async function getLoggedInUser() {
     try {
         const { account } = await createSessionClient()
 
-        const user = await account.get()
+        const result = await account.get()
+
+        const user = await getUserInfo({ userId: result.$id })
 
         return parseStringify(user)
     } catch (error) {
@@ -212,5 +238,55 @@ export const exchangePublicToken = async ({
         })
     } catch (error) {
         console.log('An error occurred while creating exchanging token:', error)
+    }
+}
+
+export const getBanks = async ({ userId }: getBanksProps) => {
+    try {
+        const { database } = await createAdminClient()
+
+        const banks = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal('userId', [userId])]
+        )
+
+        return parseStringify(banks.documents)
+    } catch (error) {
+        console.log('An error occurred while getting all user banks', error)
+    }
+}
+
+export const getBank = async ({ documentId }: getBankProps) => {
+    try {
+        const { database } = await createAdminClient()
+
+        const bank = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal('$id', [documentId])]
+        )
+
+        return parseStringify(bank.documents[0])
+    } catch (error) {
+        console.log('An error occurred while getting all user banks', error)
+    }
+}
+
+export const getBankByAccountId = async ({ accountId }: getBankByAccountIdProps) => {
+    try {
+        const { database } = await createAdminClient()
+
+        const bank = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal('accountId', [accountId])]
+        )
+
+        if (bank.total !== 1) return null
+
+        return parseStringify(bank.documents[0])
+    } catch (error) {
+        console.log('An error occurred while getting all user banks', error)
     }
 }
